7cc1a1205583478a79d9a9012ca118a1add6e292
leoBravo <98347607+leoBravoAgenda@users.noreply.github.com>
2024-03-31 17:49:06 +0200

    add aws az function

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const AWS = require("aws-sdk");
+
+// Initialize AWS SDK
+const ec2Metadata = new AWS.MetadataService();
 
 app.get("/", (req, res) => {
-  res.json({
-    name: "Leo",
-    lastName: "Brave",
-  });
+  ec2Metadata.request(
+    "/latest/meta-data/placement/availability-zone",
+    (err, data) => {
+      if (err) {
+        console.error("Error fetching instance metadata:", err);
+        res.status(500).send("Error fetching instance metadata");
+      } else {
+        const availabilityZone = data.toString();
+        res.send(`Instance is in Availability Zone: ${availabilityZone}`);
+      }
+    }
+  );
 });
 
 // Add api should return AZ later
