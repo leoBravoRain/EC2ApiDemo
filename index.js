64dd54b8e9bb3bc055d7c9527b395f568ecc85f7
leoBravo <leo.bravo.rain@gmail.com>
2024-04-01 16:55:10 +0200

    add test endpoint

--- index.js
+++ index.js
@@ -7,25 +7,26 @@ const AWS = require("aws-sdk");
 const ec2Metadata = new AWS.MetadataService();
 
 app.get("/", (req, res) => {
-  console.log("trying to access availability zone");
-  ec2Metadata.request(
-    "/latest/meta-data/placement/availability-zone",
-    (err, data) => {
-      if (err) {
-        console.error("Error fetching instance metadata:", err);
-        // res.status(500).send("Error fetching instance metadata");
-        res.status(500).json(err);
-      } else {
-        const availabilityZone = data.toString();
-        console.log("availabilty zone: ", availabilityZone);
-        res.send(`Instance is in Availability Zone: ${availabilityZone}`);
-      }
-    }
-  );
+    console.log("trying to access availability zone");
+    console.log(ec2Metadata);
+    ec2Metadata.request("/latest/meta-data/placement/availability-zone", (err, data) => {
+        if (err) {
+            console.error("Error fetching instance metadata:", err);
+            // res.status(500).send("Error fetching instance metadata");
+            res.status(500).json(err);
+        } else {
+            const availabilityZone = data.toString();
+            console.log("availabilty zone: ", availabilityZone);
+            res.send(`Instance is in Availability Zone: ${availabilityZone}`);
+        }
+    });
+});
+
+app.get("/test", (req, res) => {
+    res.status(200).json({response: 'API working'});
 });
 
 // Add api should return AZ later
-
 app.listen(port, () => {
-  console.log(`Server listening on http://localhost:${port}`);
+    console.log(`Server listening on http://localhost:${port}`);
 });
