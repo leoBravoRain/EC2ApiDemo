35013ff279dfb88c33e0b6bb87f0442ba3eaf766
leoBravo <leo.bravo.rain@gmail.com>
2024-04-01 17:09:11 +0200

    change function

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const AWS = require("aws-sdk");
+// const AWS = require("aws-sdk");
+const http = require('http');
+const os = require('os');
 
 // Initialize AWS SDK
-const ec2Metadata = new AWS.MetadataService();
+// const ec2Metadata = new AWS.MetadataService();
 
 app.get("/", (req, res) => {
-    console.log("trying to access availability zone");
-    console.log(ec2Metadata);
-    ec2Metadata.request("/latest/meta-data/placement/availability-zone", (err, data) => {
-        if (err) {
-            console.error("Error fetching instance metadata:", err);
-            // res.status(500).send("Error fetching instance metadata");
-            res.status(500).json(err);
-        } else {
-            const availabilityZone = data.toString();
-            console.log("availabilty zone: ", availabilityZone);
-            res.send(`Instance is in Availability Zone: ${availabilityZone}`);
-        }
+    // console.log("trying to access availability zone");
+    // console.log(ec2Metadata);
+    const hostname = os.hostname();
+    // const ipAddress = os.networkInterfaces()['eth0'][0]['address'];
+    // res.json({"hostname": hostname, "ipAddress": ipAddress});
+    // ec2Metadata.request("/latest/meta-data/placement/availability-zone", (err, data) => {
+    res.send(hostname)
+    //     if (err) {
+    //         console.error("Error fetching instance metadata:", err);
+    //         // res.status(500).send("Error fetching instance metadata");
+    //         res.status(500).json(err);
+    //     } else {
+    //         const availabilityZone = data.toString();
+    //         console.log("availabilty zone: ", availabilityZone);
+    //         res.send(`Instance is in Availability Zone: ${availabilityZone}`);
+    //     }
+    // });
+
+    http.get('http://169.254.169.254/latest/meta-data/', (res) => {
+        let data = '';
+
+        res.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        res.on('end', () => {
+            console.log('EC2 Instance Metadata:', data);
+        });
+    }).on('error', (err) => {
+        console.error('Error fetching EC2 instance metadata:', err);
     });
+
 });
 
 app.get("/test", (req, res) => {
